b587b698fafd38c596de545968884af3b166cf42
Ujjwal Pandey <ujjwalpandey06101998@gmail.com>
2024-10-08 14:13:49 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,24 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
-
-module.exports = { isValid };
-
-
+function isValid(s) {
+    const stack = [];
+    const bracketMap = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+    };
+  
+    for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if (char === '(' || char === '{' || char === '[') {
+        stack.push(char);
+      } else if (char === ')' || char === '}' || char === ']') {
+        if (stack.length === 0 || stack[stack.length - 1] !== bracketMap[char]) {
+          return false;
+        }
+        stack.pop();
+      }
+    }
+  
+    return stack.length === 0;
+  }
+  
+  module.exports = { isValid };
