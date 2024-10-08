ae41f108a8823c9469b9567ac266b18d1018a931
Ujjwal Pandey <ujjwalpandey06101998@gmail.com>
2024-10-08 14:19:50 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
-};
-
-
-module.exports={romanToInt}
+    const romanMap = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+    };
+  
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+      const char = s[i];
+      if (i + 1 < s.length && romanMap[char] < romanMap[s[i + 1]]) {
+        result -= romanMap[char];
+      } else {
+        result += romanMap[char];
+      }
+    }
+  
+    return result;
+  };
+  
+  module.exports = { romanToInt };
